31971b8092b0290c07ef089a419a4e9fa37e2fa6
arielperstin10 <arielperstin10@gmail.com>
2025-01-07 14:49:30 +0200

    Orders page bugs fixed

--- LibraryProject/Controllers/OrderController.cs
+++ LibraryProject/Controllers/OrderController.cs
@@ -31,16 +31,17 @@ public class OrderController : Controller
 
         var orders = await _context.Orders
             .Where(o => o.Username == username)
-            .GroupBy(o => o.OrderDate)
+            .GroupBy(o => o.OrderId) // Group by OrderId instead of OrderDate
             .Select(g => new
             {
-                OrderDate = g.Key,
-                TotalPrice = g.Sum(o => o.Price) + 10,
-                OrderIds = g.Select(o => o.OrderId).Distinct().ToList()
+                OrderId = g.Key,
+                OrderDate = g.First().OrderDate, // Take the order date from any book in the order
+                TotalPrice = g.Sum(o => o.Price * o.Quantity) + 10, // Multiply price by quantity
+                Books = g.Count() // Number of books in the order
             })
             .OrderByDescending(o => o.OrderDate)
             .ToListAsync();
-        Console.WriteLine(orders.Count);
+
         return View(orders);
     }
     
