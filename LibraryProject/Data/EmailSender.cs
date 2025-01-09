de0650d4569f52e61514161889d414ce6ca97ed1
arielperstin10 <arielperstin10@gmail.com>
2025-01-09 14:30:40 +0200

    Final Tests

--- LibraryProject/Data/EmailSender.cs
+++ LibraryProject/Data/EmailSender.cs
@@ -7,7 +7,7 @@ public class EmailSender
 {
     public async Task SendEmail(string subject, string username, string message)
     {
-        var apiKey = "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";
+        var apiKey = "";
         var client = new SendGridClient(apiKey);
         var from = new EmailAddress("ariel675345@gmail.com", "Library");
         var to = new EmailAddress("ariel675345@gmail.com", username);
